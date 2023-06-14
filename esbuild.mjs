d77365c51e05bab8670cf4b110b6b66ccf34d32e
Betarost <betarost@yandex.ru>
2023-06-14 12:10:22 +0300

    Update

--- esbuild.mjs
+++ esbuild.mjs
@@ -13,7 +13,7 @@ const assetsImages = {
     setup(build) {
         build.onResolve({ filter: /.(jpg|jpeg|png|svg)$/ }, (args) => {
             args.path = args.path.replace("@", "")
-            return { path: path.join(path.resolve("src/assets"), args.path) }
+            return { path: path.join(path.resolve("assets"), args.path) }
         })
     }
 }
@@ -22,7 +22,7 @@ const assetsFonts = {
     name: "assets-fonts",
     setup(build) {
         build.onResolve({ filter: /.(woff|woff2|eot|ttf)$/ }, (args) => {
-            return { path: path.join(path.resolve("src/assets"), args.path) }
+            return { path: path.join(path.resolve("assets"), args.path) }
         })
     }
 }
@@ -30,7 +30,7 @@ const assetsFonts = {
 const options = {
     publicPath: "/assets",
     outdir: "public/assets/",
-    entryPoints: [{ in: "app.ts", out: "js/out" }, { in: "src/assets/scss/style.scss", out: "css/out" }],
+    entryPoints: [{ in: "app.ts", out: "js/out" }, { in: "assets/scss/style.scss", out: "css/out" }],
     bundle: true,
     loader: {
         '.woff': 'file',
--- src/display.tsx
+++ src/display.tsx
@@ -1,12 +1,14 @@
-import { Cemjsx } from "cemjs-core"
+import { Cemjsx, CEM } from "cemjs-core"
 import space from '@svg/space.svg'
 
 export const display = function () {
     return (
         <div class="welcome">
             <div class="welcome-container">
-                <h2 class="welcome-title">{this.Static.text}</h2>
-                <img class="logoCem" src={space}></img>
+                <h2 class="welcome-title">{CEM.Static.text}</h2>
+                <img class="logoCem" src={space} onclick={() => {
+                    alert('Привет!')
+                }}></img>
             </div>
         </div>
     )
