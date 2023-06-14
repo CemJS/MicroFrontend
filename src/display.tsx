26ed335d9d5dd08e7c6d1e81f175cbebad26bfa1
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-06-14 16:04:46 +0300

    header, footer

--- src/display.tsx
+++ src/display.tsx
@@ -8,6 +8,21 @@ const testAlert = function () {
 export const display = function () {
     return (
         <div class="welcome">
+            <header class="header">
+                <div class="container">
+                    <div class="header_inner">
+                        <div class="header_logo">
+                            <img class="header_logo-img" src={space}></img>
+                        </div>
+                        <nav>
+                            <ul class="header_menu">
+                                <li>Cem JS</li>
+                                <li>Contacts</li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </header>
             <div class="welcome-container">
                 <h2 class="welcome-title" onclick={testAlert}>{CEM.Static.text}</h2>
                 <img class="logoCem" src={space}
@@ -15,6 +30,16 @@ export const display = function () {
                         alert("Привет!")
                     }}></img>
             </div>
+
+
+            <footer class="footer">
+                <div class="container">
+                    <div class="footer_inner">
+                        <span>Powered by Crypto Emergency</span>
+                        <span>©2023</span>
+                    </div>
+                </div>
+            </footer>
         </div>
     )
 
