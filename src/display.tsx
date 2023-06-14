f5fb60e94b7cf2f1bfd3e6ca3566156a2e37d397
Betarost <betarost@yandex.ru>
2023-06-14 13:08:48 +0300

    .

--- src/display.tsx
+++ src/display.tsx
@@ -1,14 +1,19 @@
 import { Cemjsx, CEM } from "cemjs-core"
 import space from '@svg/space.svg'
 
+const testAlert = function () {
+    alert("CEMJS!!!")
+}
+
 export const display = function () {
     return (
         <div class="welcome">
             <div class="welcome-container">
-                <h2 class="welcome-title">{CEM.Static.text}</h2>
-                <img class="logoCem" src={space} onclick={() => {
-                    alert('Привет!')
-                }}></img>
+                <h2 class="welcome-title" onclick={testAlert}>{CEM.Static.text}</h2>
+                <img class="logoCem" src={space}
+                    onclick={() => {
+                        alert("Привет!")
+                    }}></img>
             </div>
         </div>
     )
